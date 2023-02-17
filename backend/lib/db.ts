eb8a259e74e899a529943165a142fb84968ac9ba
Ramesh <ramesh-km@outlook.com>
2023-02-17 15:19:30 +0530

    chore: move public routes list to constants

--- backend/lib/db.ts
+++ backend/lib/db.ts
@@ -2,7 +2,10 @@ import { PrismaClient } from "@prisma/client";
 import { config } from "../src/config";
 
 const db = new PrismaClient({
-  log: config.NODE_ENV === "development" ? ["error",'info','warn'] : ["query"],
+  log:
+    config.NODE_ENV === "development"
+      ? ["query"]
+      : ["error", "info", "warn", "query"],
 });
 
 export default db;
